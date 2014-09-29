563bcc63ed331b8eaaf54aecb610dc6fb3fff230
Raveline <eraveline@gmail.com>
2014-09-29 13:37:20 +0200

    Removed main, made it more module-like.

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""This module give a simple API to read web pages,
+remove irrelevant html tags, get pure text out of it
+and simply render a Counter object for this words.
+External modules should only have to use the read_front_page
+method."""
 
 from bs4 import BeautifulSoup
 from collections import Counter
 import urllib.request
-import sys
 import re
 
 # Match only alphanum and hyphen (for composed words).
@@ -15,7 +18,7 @@ def read_front_page(newspaper_url):
     """Read the front page of a newspaper."""
     page = just_content(access_page(newspaper_url))
     word_count = to_word_counter(just_words(page))
-    print(word_count)
+    return word_count
 
 def access_page(url):
     """Access a page at a given URL."""
@@ -45,10 +48,3 @@ def to_word_counter(text):
     """Given a text, preprocessed so it can be easily splited
     and put in full lowercases, count occurences of words."""
     words = [w for w in just_words(text).split() if len(w) > 3]
-    return Counter(words)
-
-if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        read_front_page(sys.argv[1])
-    else:
-        print("Error. Please give url as parameter.")
