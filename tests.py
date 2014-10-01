7fdb9ebe4a90b471d8ea019c64104526d3d073e3
Raveline <eraveline@gmail.com>
2014-10-01 22:24:57 +0200

    Tests updated since feature-loss in reader.

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 from collections import Counter
-from fropag import *
+from reader import *
 
 class TestPageReader(unittest.TestCase):
     def test_access_page(self):
@@ -19,32 +19,6 @@ class TestPageReader(unittest.TestCase):
                   <script>nor this</script><p>I want this.</script>"""
         res = just_content(page)
         self.assertEqual(res, "I want this.")
-
-    def test_just_words(self):
-        basic = "The heart is a lonely hunter."
-        basic_ok = "the heart is a lonely hunter"
-        double = "The heart: a lonely hunter ?"
-        double_ok = "the heart  a lonely hunter"
-        apostrophe = "The heart's lonely hunterness"
-        apostrophe_ok = "the heart s lonely hunterness"
-        hyphen = "The lonely-hunter"
-        hyphen_ok = "the lonely-hunter"
-        unicoded = u"Un pas de sénateur élégant"
-        unicoded_ok = u"un pas de sénateur élégant"
-        
-        self.assertEqual(just_words(basic), basic_ok)
-        self.assertEqual(just_words(double), double_ok)
-        self.assertEqual(just_words(apostrophe), apostrophe_ok)
-        self.assertEqual(just_words(hyphen), hyphen_ok)
-        self.assertEqual(just_words(unicoded), unicoded_ok)
-        self.assertEqual(just_words(unicoded2), unicoded2_ok)
-
-    def test_to_word_counter(self):
-        # simple case
-        text = "this is a test test test"
-        counter = to_word_counter(just_words(text))
-        self.assertEqual(counter['this'], 1)
-        self.assertEqual(counter['test'], 3)
 
 if __name__ == "__main__":
     unittest.main()
