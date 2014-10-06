9f3c8c909e37775033adf54e1de6bf2be68f5ad7
Raveline <eraveline@gmail.com>
2014-10-06 18:51:08 +0200

    Call to see_words updated.

--- fropag.py
+++ fropag.py
@@ -21,7 +21,7 @@ def read_all(args):
     return analyze_process()
 
 def view_words(args):
-    return see_words_for(args.publication_name)
+    return see_words_for(args.publication_name, args.proper, args.limit)
 
 def add_publication(args):
     return follow_publication(args.name, args.url, args.start, args.end)
@@ -38,6 +38,8 @@ if __name__ == "__main__":
     
     words = subparsers.add_parser("words", help="Find words used by a publication")
     words.add_argument("publication_name", help="Name of the publication")
+    words.add_argument("limit", help="Max number of words", type=int)
+    words.add_argument("--proper", help="Proper nouns or commons ones", action="store_true")
 
     delete = subparsers.add_parser("delete", help = "delete help")
     delete.add_argument("id", type=int, help="Id of the front page to delete")
@@ -45,10 +47,10 @@ if __name__ == "__main__":
     init = subparsers.add_parser("init", help="Setup database.")
     read = subparsers.add_parser("read", help="Read followed front pages and analyze them.")
     follow.set_defaults(func=add_publication)
+    words.set_defaults(func=view_words)
     init.set_defaults(func=make_db)
     read.set_defaults(func=read_all)
     delete.set_defaults(func=delete_fp)
-    words.set_defaults(func=view_words)
     
     args = parser.parse_args()
 
